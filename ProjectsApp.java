a0b54cac068a127807efe2d5773e14f180cef3b6
brittneychavez <161675059+brittneychavez@users.noreply.github.com>
2024-06-23 17:45:35 -0600

    Add files via upload

--- ProjectsApp.java
+++ ProjectsApp.java
@@ -14,10 +14,15 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 //	@formatter:off
 	private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project",
+			"4) Update project details",
+			"5) Delete a project"
 			);
 //	@formatter:on
 	
@@ -41,6 +46,21 @@ public class ProjectsApp {
 				case 1:
 					createProject();
 					break;
+					
+				case 2:
+					listProjects();
+					break;
+					
+				case 3:
+					selectProject();
+					break;
+				case 4:
+					updateProjectDetails();
+					break;
+				case 5:
+					deleteProject();
+					break;
+					
 				default:
 					System.out.println("\n" + selection + " is not a valid selecction. Try again.");
 					break;
@@ -50,6 +70,76 @@ public class ProjectsApp {
 			catch(Exception e) {
 				System.out.println("\nEror: " + e + " Try again.");
 			}
+		}
+	}
+	
+	private void updateProjectDetails() {
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nPlease select a project.");
+			return;
+		}
+
+	
+	
+		String projectName =
+				getStringInput("Enter the project name [" + curProject.getProjectName() + "]");
+		BigDecimal estimatedHours =
+				getDecimalInput("Enter the estimated hours [" + curProject.getEstimatedHours() + "]");
+		BigDecimal actualHours = 
+				getDecimalInput("Enter the actual hours + [" + curProject.getActualHours() + "]");
+		Integer difficulty = 
+				getIntInput("Enter the project difficulty (1-5) [" + curProject.getDifficulty() + "]");
+		String notes = 
+				getStringInput("Enter the project notes [" + curProject.getNotes() + "]");
+		Project project = new Project();
+		
+		project.setProjectId(curProject.getProjectId());
+		project.setProjectName(Objects.isNull(projectName) ? curProject.getProjectName() : projectName);
+		
+		project.setEstimatedHours(
+				Objects.isNull(estimatedHours) ? curProject.getEstimatedHours() : estimatedHours);
+		
+		project.setActualHours (Objects.isNull(actualHours) ? curProject.getActualHours() : actualHours);
+		project.setDifficulty(Objects.isNull(difficulty) ? curProject.getDifficulty() : difficulty);
+		project.setNotes(Objects.isNull(notes) ? curProject.getNotes() : notes);
+		
+		projectService.modifyProjectDetails(project);
+		
+		curProject = projectService.fetchProjectById(curProject.getProjectId());
+	}
+private void deleteProject() {
+	listProjects();
+	
+	Integer projectId = getIntInput("Enter the ID of the project to delete");
+	
+	projectService.deleteProject(projectId);
+	System.out.println("Project " + projectId + " was deleted successfully.");
+	
+	if(Objects.nonNull(curProject) && curProject.getProjectId().equals(projectId)) {
+		curProject = null;
+	}
+}
+private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		curProject = projectService.fetchProjectById(projectId);
+	}
+	
+	private void listProjects() {
+		List<Project> projects= projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out
+				.println("   " + project.getProjectId() + ": " + project.getProjectName()));
+		
+		if (Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with project: " + curProject);
 		}
 	}
 
@@ -120,5 +210,11 @@ private String getStringInput(String prompt) {
 private void printOperations() {
 	System.out.println("\nThese are the available selections. Press the Enter key to quit:");
 	operations.forEach(line -> System.out.println(" " + line));
+	
+	if (Objects.isNull(curProject)) {
+		System.out.println("\nYou do not have an active project.");
+	} else {
+		System.out.println("\n You are viewing: " + curProject);
+	}
 }
 }
